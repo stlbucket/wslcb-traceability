f646e8ef9175beb9598517bde0846669c03114c1
Kevin Burkett <stlbucket@gmail.com>
2020-05-15 23:14:03 -0700

    login manager tweak

--- web-vue/src/components/_common/LoginManager/LoginManagerVuetify.vue
+++ web-vue/src/components/_common/LoginManager/LoginManagerVuetify.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-btn @click="login" :hidden="isLoggedIn">Login</v-btn>
+    <!-- <v-btn @click="login" :hidden="isLoggedIn">Login</v-btn> -->
     <v-btn @click="logout" :hidden="!isLoggedIn">Logout</v-btn>
   </div>
 </template>
